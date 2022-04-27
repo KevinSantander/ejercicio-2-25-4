44f65b293896ef345520ac32246db8d200fda5ed
Kevin <104107277+KevinSantander@users.noreply.github.com>
2022-04-27 17:41:38 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ejercicio-2-25-4
+# trabajo-2-Dato-24-4-22
 Created with CodeSandbox
---
+++ ej25-4/datos.ts
@@ -0,0 +1,12 @@
+// resolucion del Ejercicio
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  // TUCODIGO VA AQUI, POR EJEMPLO:
+  console.log("el dato ingresado es", dato.value);
+  //
+});
+
+rotulo.innerHTML = "ingrese un dato: ";
